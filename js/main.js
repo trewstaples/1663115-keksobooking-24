2fcfc4a3c0be571932e78de191713deed6729721
LAlex <trewstaples@gmail.com>
2021-10-01 12:55:00 +0300

    Добавит функции генерации случайных чисел

---
+++ js/main.js
@@ -0,0 +1,26 @@
+const getRandomInteger = (min, max) => {
+  if (min < 0 || max < 0) {
+    return -1;
+  }
+
+  if (min > max) {
+    [min, max] = [max, min];
+  }
+
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+getRandomInteger();
+
+const getRandomPositiveFloat = (min, max, decimalCount) => {
+  if (min < 0 || max < 0) {
+    return -1;
+  }
+
+  if (min > max) {
+    [min, max] = [max, min];
+  }
+  const number = Math.random() * (max - min + 1) + min;
+  return number.toFixed(decimalCount);
+};
+getRandomPositiveFloat();
