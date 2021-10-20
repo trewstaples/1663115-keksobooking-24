ac65c2454a0d7a8bda4f1fa301b5cf913fb7dac7
LAlex <trewstaples@gmail.com>
2021-10-20 13:32:13 +0300

    Добавит функции для каждого ключа объекта объявления

--- js/adverts.js
+++ js/adverts.js
@@ -1,30 +1,136 @@
 import { adverts } from './data.js';
 
-//Найти шаблон для заполнения данных и создать фрагмент
-const advertsTemplate = document.querySelector('#card').content.querySelector('.popup');
-advertsTemplate;
-const advertsFragment = document.createDocumentFragment();
-advertsFragment;
-
-//Найти блок карты для вставки сгенерированных данных
+//Найти блок, куда будут вставляться данные - OK
 const advertsMap = document.querySelector('#map-canvas');
 advertsMap;
 
+//Создать фрагмент, куда будем складывать данные
+const advertsFragment = document.createDocumentFragment();
+advertsFragment;
+
+//Найти шаблон для заполнения данными
+const advertsTemplate = document.querySelector('#card').content.querySelector('.popup');
+advertsTemplate;
+
+//Функция, добавляющая заголовок
+const fillTitle = (markupElement, attribute, data) => {
+  if (data === '' || data === null || data === undefined) {
+    markupElement.querySelector(attribute).classList.add('hidden');
+  } else {
+    markupElement.querySelector(attribute).textContent = data;
+  }
+};
+
+//Функция, добавляющая адресс
+const fillAdress = (markupElement, attribute, data) => {
+  if (data === '' || data === undefined || data === null) {
+    markupElement.querySelector(attribute).classList.add('hidden');
+  } else {
+    markupElement.querySelector(attribute).textContent = data;
+  }
+};
+
+//Функция, добавляющая цену
+const fillPrice = (markupElement, attribute, data) => {
+  if (data === '' || data === undefined || data === null) {
+    markupElement.querySelector(attribute).classList.add('hidden');
+  } else {
+    markupElement.querySelector(attribute).textContent = `${data} ₽/ночь`;
+  }
+};
+
+//Функция, добавляющая тип
+const fillType = (markupElement, attribute, data, type) => {
+  if (data === '' || data === null || data === undefined) {
+    markupElement.querySelector(attribute).classList.add('hidden');
+  } else {
+    markupElement.querySelector(attribute).textContent = type;
+  }
+};
+
+const getOfferType = (type) => {
+  switch (type) {
+    case 'flat':
+      return 'Квартира';
+    case 'bungalow':
+      return 'Бунгало';
+    case 'house':
+      return 'Дом';
+    case 'palace':
+      return 'Дворец';
+    case 'hotel':
+      return 'Отель';
+  }
+};
+
+const fillTextCapacity = (markupElement, attribute, roomsData, guestsData) => {
+  if (roomsData === '' || roomsData === null || roomsData === undefined || guestsData === '' || guestsData === null || guestsData === undefined) {
+    markupElement.querySelector(attribute).classList.add('hidden');
+  } else {
+    markupElement.querySelector(attribute).textContent = `${roomsData} комнаты для ${guestsData} гостей`;
+  }
+};
+
+const fillPopupTextTime = (markupElement, attribute, checkinData, checkoutData) => {
+  if (checkinData === '' || checkinData === null || checkinData === undefined || checkoutData === '' || checkoutData === null || checkoutData === undefined) {
+    markupElement.querySelector(attribute).classList.add('hidden');
+  } else {
+    markupElement.querySelector(attribute).textContent = `Заезд после ${checkinData}, выезд после ${checkoutData}`;
+  }
+};
+
+const fillPopupFeatures = (markupElement, attribute, data) => {
+  if (data === '' || data === null || data === undefined) {
+    markupElement.querySelector(attribute).classList.add('hidden');
+  } else {
+    markupElement.querySelector(attribute).textContent = data;
+  }
+};
+
+const fillPopupDescription = (markupElement, attribute, data) => {
+  if (data === '' || data === null || data === undefined) {
+    markupElement.querySelector(attribute).classList.add('hidden');
+  } else {
+    markupElement.querySelector(attribute).textContent = data;
+  }
+};
+
+const fillPopupAvatar = (markupElement, attribute, data) => {
+  if (data === '' || data === null || data === undefined) {
+    markupElement.querySelector(attribute).classList.add('hidden');
+  } else {
+    markupElement.querySelector(attribute).src = `img/avatars/user${data}.png`;
+  }
+};
 //Вставить данные из массива в шаблон.
-adverts.forEach((advert) => {
+const createAdvert = (advert) => {
+  const { author, offer } = advert;
   const newAdvert = advertsTemplate.cloneNode(true);
 
-  newAdvert.querySelector('.popup__title').textContent = advert.offer.title;
-  newAdvert.querySelector('.popup__text--address').textContent = advert.offer.address;
-  newAdvert.querySelector('.popup__text--price').textContent = `${advert.price} ₽/ночь`;
-  newAdvert.querySelector('.popup__type').textContent = advert.offer.type; //Как сделать деление на типы?
-  newAdvert.querySelector('.popup__text--capacity').textContent = `${advert.offer.rooms} комнаты для ${advert.offer.guests} гостей`;
-  newAdvert.querySelector('.popup__text--time').textContent = `Заезд после ${advert.offer.checkin}, выезд после ${advert.offer.checkout}`;
-  newAdvert.querySelector('.popup__features').textContent = advert.offer.features;
-  newAdvert.querySelector('.popup__description').textContent = advert.offer.description;
-  newAdvert.querySelector('.popup__photos').src = advert.offer.photos;
-  newAdvert.querySelector('.popup__avatar').src = advert.author.avatar;
+  for (let i = 0; i < 1; i++) {
+    fillTitle(newAdvert, '.popup__title', offer.title);
+    fillAdress(newAdvert, '.popup__text--address', offer.address);
+    fillPrice(newAdvert, '.popup__text--price', offer.price);
+    fillType(newAdvert, '.popup__type', offer.type, getOfferType(offer.type));
+    fillTextCapacity(newAdvert, '.popup__text--capacity', offer.rooms, offer.guests);
+    fillPopupTextTime(newAdvert, '.popup__text--time', offer.checkin, offer.checkout);
+    fillPopupFeatures(newAdvert, '.popup__features', offer.features);
+    fillPopupDescription(newAdvert, '.popup__description', offer.description);
+    fillPopupAvatar(newAdvert, '.popup__avatar', author.avatar);
 
-  advertsFragment.appendChild(newAdvert);
-});
+    const photosList = newAdvert.querySelector('.popup__photos');
+    const photoFragment = document.createDocumentFragment();
+
+    offer.photos.forEach((photo) => {
+      const popupPhoto = photosList.querySelector('.popup__photo').cloneNode(true);
+      popupPhoto.src = photo.src;
+      photoFragment.appendChild(popupPhoto);
+    });
+
+    photosList.innerHTML = '';
+    photosList.appendChild(photoFragment);
+    advertsFragment.appendChild(newAdvert);
+  }
+};
+createAdvert(adverts[0]);
 advertsMap.appendChild(advertsFragment);
