3ec68aa0ccd600132bf18bd62ec575e62183f200
LAlex <trewstaples@gmail.com>
2021-10-25 14:09:33 +0300

    Добавит логики активации

--- js/form.js
+++ js/form.js
@@ -1,12 +1,13 @@
 const adForm = document.querySelector('.ad-form');
 const mapFilters = document.querySelector('.map__filters');
 
-const deactivateForm = () => {
+const deactivatePage = () => {
   adForm.classList.add('ad-form--disabled');
 
   for (let i = 0; i < adForm.children.length; i++) {
     adForm.children[i].setAttribute('disabled', true);
   }
+
   mapFilters.classList.add('map__filters--disabled');
 
   for (let j = 0; j < mapFilters.children.length; j++) {
@@ -14,8 +15,30 @@ const deactivateForm = () => {
   }
 };
 
-const activatePage = (condition) => {
-  condition ? '' : deactivateForm();
+const activatePage = () => {
+  adForm.classList.remove('ad-form--disabled');
+
+  for (let k = 0; k < adForm.children.length; k++) {
+    adForm.children[k].removeAttribute('disabled', true);
+  }
+  mapFilters.classList.remove('map__filters--disabled');
+
+  for (let l = 0; l < mapFilters.children.length; l++) {
+    mapFilters.children[l].removeAttribute('disabled', true);
+  }
 };
 
-activatePage(1);
+const mapCanvas = document.querySelector('.map__canvas');
+
+mapCanvas.addEventListener('mousedown', (evt) => {
+  if (evt.button === 0) {
+    activatePage();
+  }
+});
+
+mapCanvas.addEventListener('keydown', (evt) => {
+  if (evt.key === 'Enter') {
+    activatePage();
+  }
+});
+deactivatePage();
