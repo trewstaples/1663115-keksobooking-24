891f2e7d62a872875999e42eb4dbb4edddf56be7
LAlex <trewstaples@gmail.com>
2021-11-08 23:15:35 +0300

    Добавит фильтрацию по числу комнат и количеству гостей

--- js/get-data.js
+++ js/get-data.js
@@ -64,9 +64,50 @@ const onDownloadSuccess = (data) => {
   const housingPrice = document.querySelector('#housing-price');
   housingPrice.addEventListener('change', (evt) => {
     const priceOfHouse = evt.target.value;
-    console.log(priceOfHouse);
     deleteMarkers();
     const flatAdverts = adverts.filter((advert) => getPrice(priceOfHouse, advert.offer.price));
+    renderMarkers(flatAdverts.slice(0, ADVERT_COUNT));
+  });
+
+  const getRooms = (option, rooms) => {
+    switch (option) {
+      case 'any':
+        return rooms;
+      case '1':
+        return rooms === 1;
+      case '2':
+        return rooms === 2;
+      case '3':
+        return rooms === 3;
+    }
+  };
+
+  const housingRooms = document.querySelector('#housing-rooms');
+  housingRooms.addEventListener('change', (evt) => {
+    const roomsOfHouse = evt.target.value;
+    deleteMarkers();
+    const flatAdverts = adverts.filter((advert) => getRooms(roomsOfHouse, advert.offer.rooms));
+    renderMarkers(flatAdverts.slice(0, ADVERT_COUNT));
+  });
+
+  const getGuests = (option, guests) => {
+    switch (option) {
+      case 'any':
+        return guests;
+      case '1':
+        return guests === 1;
+      case '2':
+        return guests === 2;
+      case '0':
+        return guests === 0;
+    }
+  };
+
+  const housingGuests = document.querySelector('#housing-guests');
+  housingGuests.addEventListener('change', (evt) => {
+    const guestsOfHouse = evt.target.value;
+    deleteMarkers();
+    const flatAdverts = adverts.filter((advert) => getGuests(guestsOfHouse, advert.offer.guests));
     renderMarkers(flatAdverts.slice(0, ADVERT_COUNT));
   });
 };
